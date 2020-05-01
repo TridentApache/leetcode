781eef2e17794c3e9132336fd216736e31630951
Le Wang <lewang95@outlook.com>
2020-05-01 12:51:57 +0800

    更新leetcode066题解和思路

--- src/N066/leetcode066.java
+++ src/N066/leetcode066.java
@@ -5,13 +5,52 @@ import java.util.Arrays;
 public class leetcode066 {
     public static void main(String[] args){
         Solution s = new Solution();
-        int[] nums1=new int[]{9,9};
-        s.plusOne(nums1);
-        System.out.println(Arrays.toString(nums1));
+        int[] nums1=new int[]{1,1};
+        System.out.println(Arrays.toString(s.plusOne1(nums1)));
     }
 }
 
 class Solution {
+    public int[] plusOne1(int[] digits) {
+        /**
+         * 思路和第67题的二进制相加的位操作有点像，第67题2个步骤循环操作直到无进位，这题也相加直到没有进位
+         * */
+        int i=digits.length-1;
+        int flag=1;//加一看成进位
+        while(i>=0 && flag!=0){
+            digits[i]+=flag;
+            flag=(digits[i])/10;
+            digits[i]%=10;
+            i--;
+        }
+        if(i<=0&&flag!=0){
+            digits = new int [digits.length+1];
+            digits[0]=1;
+        }
+        return digits;
+    }
+
+    /**
+     * 别人更精炼的代码！
+     *     作者：guanpengchn
+     *     链接：https://leetcode-cn.com/problems/plus-one/solution/hua-jie-suan-fa-66-jia-yi-by-guanpengchn/
+     *     来源：力扣（LeetCode）
+     *     著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * */
+    public int[] plusOne2(int[] digits) {
+        int len = digits.length;
+        for(int i = len - 1; i >= 0; i--) {
+            digits[i]++;
+            digits[i] %= 10;
+            if(digits[i]!=0)
+                return digits;
+        }
+        digits = new int[len + 1];
+        digits[0] = 1;
+        return digits;
+    }
+
+
     public int[] plusOne(int[] digits) {
         int len = digits.length;
         int flag;
@@ -31,14 +70,6 @@ class Solution {
                 flag=0;
             }
         }
-        // if(digits[0]==0){
-        //     int[] res = new int[len+1];
-        //     res[0]=1;
-        //     for(int i=1;i<len+1;i++){
-        //         res[i]=digits[i-1];
-        //     }
-        //     digits=res;
-        // }
         if(digits[0]==0){
             digits = new int[len+1];
             digits[0]=1;

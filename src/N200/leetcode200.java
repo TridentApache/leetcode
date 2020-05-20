40751a04d3255c8f48413b3a680c5e91fbb4ac06
Le Wang <lewang95@outlook.com>
2020-05-20 11:11:22 +0800

    添加leetcode200题解

---
+++ src/N200/leetcode200.java
@@ -0,0 +1,99 @@
+package N200;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class leetcode200 {
+    public static void main(String[] args){
+        Solution s = new Solution();
+        char[][] test=new char[][]{{'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}};
+        s.numIslands(test);
+    }
+}
+/**
+ * 给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+ * 岛屿总是被水包围，并且每座岛屿只能由水平方向或竖直方向上相邻的陆地连接形成。
+ * 此外，你可以假设该网格的四条边均被水包围。
+ *
+ * 输入:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ * 输出: 1
+ *
+ * 输入:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ * 输出: 3
+ * */
+
+class Solution {
+    int count=0;
+    int row;
+    int col;
+    boolean [][] visited;
+    LinkedList<int[]> queue;
+    public int numIslands(char[][] grid) {
+        if(grid.length==0) return 0;
+        return BFS(grid);
+    }
+    public int BFS(char[][] grid){
+        queue = new LinkedList<int[]>();
+        row = grid.length;
+        col = grid[0].length;
+        visited = new boolean[row][col];
+        for(int i=0;i<row;i++) {
+            for (int j = 0; j < col; j++) {
+                if (grid[i][j] == '1' && visited[i][j]==false) {
+                    queue.add(new int[]{i, j});
+                    visited[i][j]=true;
+                    count++;
+                    tmp(grid);
+                }
+            }
+        }
+        return count;
+    }
+    public void tmp(char[][] grid){
+        while(queue.size()!=0){
+            int len = queue.size();
+            for(int k=0;k<len;k++){
+                int[] tmp = queue.removeFirst();
+                int i = tmp[0];
+                int j= tmp[1];
+
+                if(i-1>=0 && grid[i-1][j]=='1' && visited[i-1][j]==false){
+                        queue.addLast(new int[]{i - 1, j});
+                        visited[i - 1][j] = true;
+
+                }
+
+                if(j-1>=0 && grid[i][j-1]=='1' && visited[i][j-1]==false){
+                        queue.addLast(new int[]{i,j-1});
+                        visited[i][j-1]=true;
+                }
+
+                if(i+1<row && grid[i+1][j]=='1' && visited[i+1][j]==false){
+                        queue.addLast(new int[]{i+1,j});
+                        visited[i+1][j]=true;
+                }
+
+                if(j+1<col && grid[i][j+1]=='1' && visited[i][j+1]==false){
+                        queue.addLast(new int[]{i,j+1});
+                        visited[i][j+1]=true;
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
